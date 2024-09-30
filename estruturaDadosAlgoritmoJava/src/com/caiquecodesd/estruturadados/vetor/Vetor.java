feabe3e999f56fcc1416c3a83d50d8b9761f79aa
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-09-30 20:34:43 -0300

    Create Vetor.java

---
+++ estruturaDadosAlgoritmoJava/src/com/caiquecodesd/estruturadados/vetor/Vetor.java
@@ -0,0 +1,42 @@
+package com.caiquecodesd.estruturadados.vetor;
+
+public class Vetor {
+
+	private String[] elementos;
+	private int tamanho;
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];	
+	}
+
+	/*public void adiciona(String elemento) {
+		for (int i = 0; i < this.elementos.length; i++) {
+			if (this.elementos[i] == null) {
+				this.elementos[i] = elemento;
+				this.tamanho = 0;
+				break;
+			}
+		}
+	}*/
+
+	/*public void adiciona(String elemento) throws Exception {
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		} else {
+			throw new Exception("Vetor cheio, não é possível adicionar novos elementos.");
+		}
+
+	}*/
+
+	public boolean adiciona(String elemento) {
+
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}
+		return false;
+	}
+
+}
