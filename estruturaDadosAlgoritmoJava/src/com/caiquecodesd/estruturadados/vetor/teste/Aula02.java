2f005780333fdc46b6be44a12b4a5af1b7aa3719
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-09-30 20:36:27 -0300

    Create Aula02.java

---
+++ estruturaDadosAlgoritmoJava/src/com/caiquecodesd/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,13 @@
+package com.caiquecodesd.estruturadados.vetor.teste;
+
+import com.caiquecodesd.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(3);
+		
+	}
+	
+}
