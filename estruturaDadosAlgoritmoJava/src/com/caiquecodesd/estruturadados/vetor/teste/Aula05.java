9b837daa42765b8d599ff9d3f56fdf9dae7c8cfb
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-10-01 20:36:05 -0300

    Create Aula05.java
    Obter elemento de uma posição

---
+++ estruturaDadosAlgoritmoJava/src/com/caiquecodesd/estruturadados/vetor/teste/Aula05.java
@@ -0,0 +1,21 @@
+package com.caiquecodesd.estruturadados.vetor.teste;
+
+import com.caiquecodesd.estruturadados.vetor.Vetor;
+
+public class Aula05 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(7);
+
+		vetor.adiciona("Elemento 1");
+		vetor.adiciona("Elemento 2");
+		vetor.adiciona("Elemento 3");
+		vetor.adiciona("Elemento 4");
+		vetor.adiciona("Elemento 5");
+		
+		System.out.println(vetor.busca(18));
+		
+	}
+	
+}
