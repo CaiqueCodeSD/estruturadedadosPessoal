4080804053466f10c40dbc788dd40c7755dd7533
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-10-02 20:06:46 -0300

    Create Aula08.java

---
+++ estruturaDadosAlgoritmoJava/src/com/caiquecodesd/estruturadados/vetor/teste/Aula08.java
@@ -0,0 +1,23 @@
+package com.caiquecodesd.estruturadados.vetor.teste;
+
+import com.caiquecodesd.estruturadados.vetor.Vetor;
+
+public class Aula08 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(3);
+
+		vetor.adiciona("Aa");
+		vetor.adiciona("Bb");
+		vetor.adiciona("Cc");
+		vetor.adiciona("Dd");
+		vetor.adiciona("Ee");
+		vetor.adiciona("Ff");
+		vetor.adiciona("Gg");
+		
+		System.out.println(vetor);
+
+	}
+
+}
