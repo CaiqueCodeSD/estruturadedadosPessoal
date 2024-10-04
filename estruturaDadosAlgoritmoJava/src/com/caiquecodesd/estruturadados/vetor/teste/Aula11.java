44402dcc49211a5eb508a4828bb53c68f611f77d
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-10-03 21:19:20 -0300

    Define o tipo do vetor Dinamicamente <T>

---
+++ estruturaDadosAlgoritmoJava/src/com/caiquecodesd/estruturadados/vetor/teste/Aula11.java
@@ -0,0 +1,15 @@
+package com.caiquecodesd.estruturadados.vetor.teste;
+
+import com.caiquecodesd.estruturadados.vetor.Lista;
+
+public class Aula11 {
+
+    public static void main(String[] args) {
+        
+        Lista<String> vetor = new Lista<String>(1);
+
+        vetor.adiciona("Guts");
+        
+    }
+
+}
